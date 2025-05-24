ab40b8dc4ced6cb8dd9334332b1103875adc42f6
crushed-lemon <crushed.lemon.official@gmail.com>
2025-05-24 15:34:29 +0530

    Use Tailwind for Css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 const Home: React.FunctionComponent = () => {
   return (
-    <div>
+    <div className="bg-indigo-500 text-white p-6 rounded-xl">
         Youtube Home.
     </div>
   );
